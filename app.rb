1129e7e8c111e684ec90e35e7d336276e683f289
audipod <146144639+audipod@users.noreply.github.com>
2023-10-15 05:30:31 +0000

    added a lot

--- app.rb
+++ app.rb
@@ -5,3 +5,50 @@ get("/") do
     erb(:rules)
   end
 
+  get("/rock") do
+    @rolls = []
+  
+    2.times do
+      options = ["Rock", "Paper", "Scissors"]
+
+      # Choose a random play from the options
+      play = options.sample
+      
+      # Add the play to the @rolls array
+      @rolls.push(play)
+    end
+  
+    erb(:rock)
+  end
+
+  get("/paper") do
+    @rolls = []
+  
+    2.times do
+      options = ["Rock", "Paper", "Scissors"]
+
+      # Choose a random play from the options
+      play = options.sample
+      
+      # Add the play to the @rolls array
+      @rolls.push(play)
+    end
+
+    erb(:paper)
+  end
+
+  get("/scissors") do
+    @rolls = []
+  
+    2.times do
+      options = ["Rock", "Paper", "Scissors"]
+
+      # Choose a random play from the options
+      play = options.sample
+      
+      # Add the play to the @rolls array
+      @rolls.push(play)
+    end
+
+    erb(:scissors)
+  end
